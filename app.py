ec138a74dcff30fe67e954c1c620573fb8d50138
luna <luna@hackclub.app>
2025-03-12 22:17:37 +0800

    devmode toggle

--- app.py
+++ app.py
@@ -17,5 +17,9 @@ def syntax():
     return render_template('syntax.html')
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=7272)
+    devmode = True
+    if devmode:
+        app.run(debug=True)
+    else:
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=7272)
