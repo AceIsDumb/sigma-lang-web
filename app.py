d1e51c57a000d9ffdefbe52ab5863acc8eaccd1f
luna <luna@hackclub.app>
2025-03-12 21:46:13 +0800

    removed quickstart.html because its uneeded + moved css to one stylesheet!! amazing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import *
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -11,15 +11,10 @@ def homepage():
 def diminipage():
     return render_template('dimini.html')
 
-@app.route('/quickstart/')
-@app.route('/quickstart')
-def quickstart():
-    return render_template('quickstart.html')
-
 @app.route('/syntax/')
 @app.route('/syntax')
 def syntax():
     return render_template('syntax.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='7272', debug = True)
+    app.run(host='0.0.0.0', port=7272, debug = True)
